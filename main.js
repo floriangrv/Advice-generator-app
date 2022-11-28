773c6e0d758570cacd26a51ebcc187e2ade21e5e
floriangrv <florian.gravepro@gmail.com>
2022-11-29 00:51:04 +0100

    add hover btn

--- main.js
+++ main.js
@@ -2,35 +2,35 @@ const adviceId = document.querySelector(".advice-id");
 const adviceText = document.querySelector(".advice-text");
 const btn = document.querySelector("#advice-btn");
 
-console.log(btn)
+console.log(btn);
 
 function getAdvice() {
-    fetch('https://api.adviceslip.com/advice').then(response => {
-        return response.json();
-    }).then(adviceData => {
-        const adviceIdd = adviceData.slip.id;
-        const advice = adviceData.slip.advice;
-        btn.classList.add('rotate-center')
-      
-        adviceId.textContent = adviceIdd;
-        adviceText.textContent = advice;
-    }).catch(error => {
-        console.log(error);
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => {
+      return response.json();
     })
+    .then((adviceData) => {
+      const adviceIdd = adviceData.slip.id;
+      const advice = adviceData.slip.advice;
+      btn.classList.add("rotate-center");
+
+      adviceId.textContent = adviceIdd;
+      adviceText.textContent = advice;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-btn.addEventListener('click', () => {
-    btn.classList.add('rotate-center')
-    getAdvice();
-    
-    setTimeout(() => {
-        btn.classList.remove('rotate-center')
-    }, 1500)
-    
+btn.addEventListener("click", () => {
+  btn.classList.add("rotate-center");
+  getAdvice();
+
+  setTimeout(() => {
+    btn.classList.remove("rotate-center");
+  }, 1500);
 });
 
 window.onload = () => {
-    getAdvice();
-}
-
-
+  getAdvice();
+};
